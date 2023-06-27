b1a5c3f24629b605a89f2e6551854d4b09f98573
Sergey Malyarov <sergey@malyaroff.com>
2023-06-27 13:48:01 +0300

    hw4

--- homework-2/main.py
+++ homework-2/main.py
@@ -1,4 +1,5 @@
 from src.channel import Channel
+from src.video import Video, PLVideo
 
 if __name__ == '__main__':
     moscowpython = Channel('UC-OVMPlMA3-YCIeg4z5z23A')
@@ -20,3 +21,13 @@ if __name__ == '__main__':
 
     # создаем файл 'moscowpython.json' в данными по каналу
     moscowpython.to_json('moscowpython.json')
+
+
+if __name__ == '__main__':
+    # Создаём экземпляр класса Video
+    video1 = Video('videoID1', 'Awesome video', 'https://www.youtube.com/watch?v=dQw4w9WgXcQ', 1000, 500)
+    print(video1)  # Video('videoID1', 'Awesome video', 'https://www.youtube.com/watch?v=dQw4w9WgXcQ', 1000, 500)
+
+    # Создаём экземпляр класса PLVideo
+    pl_video1 = PLVideo('videoID2', 'Another awesome video', 'https://www.youtube.com/watch?v=3t6bLugtJkQ', 2000, 1200, 'playlistID1')
+    print(pl_video1)  # PLVideo('videoID2', 'Another awesome video', 'https://www.youtube.com/watch?v=3t6bLugtJkQ', 2000, 1200, 'playlistID1')
